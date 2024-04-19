bf4ff04f6840c15f928ec42d0de1b599052c3532
Nikita Deyneko <redfastt6@gmail.com>
2024-04-19 19:08:37 +0600

    мидлвары

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require("express");
-
+const { cors } = require("./middlewares/cors")
+const bodyParser = require("body-parser");
+const path = require("path");
 const mainRoute = require("./routes/main");
 const gamesRouter = require("./routes/games");
 
 const PORT = 3000;
 const app = express();
 
-app.use(mainRoute, gamesRouter);
+
+
+app.use(
+    cors,
+    bodyParser.json(),
+    express.static(path.join(__dirname, "public")),
+    mainRoute,
+    gamesRouter
+);
 
 app.listen(PORT, () => {
     console.log(`Server is running at PORT http://localhost:${PORT}`);
--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,10 @@
 const mainRoute = require("express").Router();
+const fs = require("fs").promises;
 
 mainRoute.get("/", (req, res) => {
-    res.status(200).send("<h1>Скоро тут будет интерфейс</h1>");
+    fs.readFile("./public/index.html", "utf-8").then((data) => {
+    res.header("Content-Type", "text/html").send(data);
+    });
 });
 
 module.exports = mainRoute;
