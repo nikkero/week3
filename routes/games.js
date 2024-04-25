49cf9c9126dc56448305fd06bc2e15516856fa6b
Nikita Deyneko <redfastt6@gmail.com>
2024-04-25 19:33:08 +0600

    database

--- routes/games.js
+++ routes/games.js
@@ -1,13 +1,10 @@
+// Файл routes/games.js
+
 const gamesRouter = require("express").Router();
-const { getAllGames, findGameById, deleteGame, checkIsTitleInArray, updateGamesArray, updateGamesFile, } = require("../middlewares/games");
 
-const {
-    sendAllGames,
-    sendUpdatedGames
-} = require("../controllers/games");
+const findAllGames = require("../middlewares/games");
+const sendAllGames = require("../controllers/games");
 
-gamesRouter.post("/games", getAllGames, checkIsTitleInArray, updateGamesArray, updateGamesFile, sendUpdatedGames);
-gamesRouter.get("/games", getAllGames, sendAllGames);
-gamesRouter.delete("/games/:id", getAllGames, findGameById, deleteGame, updateGamesFile, sendUpdatedGames);
+gamesRouter.get("/games", findAllGames, sendAllGames);
 
 module.exports = gamesRouter;
---
+++ routes/gamesOld.js
@@ -0,0 +1,13 @@
+// const gamesRouter = require("express").Router();
+// const { getAllGames, findGameById, deleteGame, checkIsTitleInArray, updateGamesArray, updateGamesFile, } = require("../middlewares/games");
+
+// const {
+//     sendAllGames,
+//     sendUpdatedGames
+// } = require("../controllers/games");
+
+// gamesRouter.post("/games", getAllGames, checkIsTitleInArray, updateGamesArray, updateGamesFile, sendUpdatedGames);
+// gamesRouter.get("/games", getAllGames, sendAllGames);
+// gamesRouter.delete("/games/:id", getAllGames, findGameById, deleteGame, updateGamesFile, sendUpdatedGames);
+
+// module.exports = gamesRouter;
