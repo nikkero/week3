43bbf1cb1817b8367f6a14c0ce87f8df4be4a1a7
Nikita Deyneko <redfastt6@gmail.com>
2024-05-21 19:33:29 +0600

    update BASE_URL and CORS

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,20 +1,24 @@
-function cors(req, res, next) {
-    const { origin } = req.headers;
-    if (allowedCorsOrigin.includes(origin)) {
-        res.header("Access-Control-Allow-Origin", origin);
-        res.header(
-            "Access-Control-Allow-Headers",
-            "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-        );
-        res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    }
-
-    next();
-}
-const allowedCorsOrigin = [
-    "https://practicum.yandex.ru",
-    "https://students-projects.ru",
+const allowedCors = [
+    "https://pindiegg.nomoredomainswork.ru",
     "http://localhost:3000",
 ];
 
-module.exports = { cors };
+function cors(req, res, next) {
+    const { origin } = req.headers;
+
+    if (allowedCors.includes(origin)) {
+        res.header("Access-Control-Allow-Origin", origin);
+    }
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,PUT,PATCH,POST,DELETE"
+    );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+    );
+    next();
+}
+
+module.exports = cors; 
+
